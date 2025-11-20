8f5298686a27c8fb0591c863e37d58486b941344
GRSdzn <pochtagrs@mail.ru>
2025-11-20 20:24:20 +0300

    feat: add viewport configuration and clean up layout imports
    - Introduced viewport configuration to define theme color for improved user experience.
    - Removed duplicate import of globals.scss to streamline the layout file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import '../styles/globals.scss';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { Inter } from 'next/font/google';
-import '../styles/globals.scss';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { mainLayoutMeta } from '@/components/seo/meta';
+import type { Viewport } from 'next';
 
 const bebasNeueCyrillic = localFont({
   src: '../../public/fonts/bebas-neue-regular.woff2',
@@ -18,6 +19,9 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
+export const viewport: Viewport = {
+  themeColor: '#151515',
+};
 export const metadata: Metadata = mainLayoutMeta;
 
 export default function RootLayout({
