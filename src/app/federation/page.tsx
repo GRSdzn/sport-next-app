795d5cf3d57af3a5180a549bb4c37995511c93a8
GRSdzn <pochtagrs@mail.ru>
2025-11-22 15:28:43 +0300

    feat: add federation metadata and remove contacts metadata
    - Introduced federationMeta for SEO, providing detailed information about the federation's goals and structure.
    - Updated FederationPage to utilize the new metadata.
    - Removed contactsMeta as it is no longer needed, streamlining the SEO configuration.

--- src/app/federation/page.tsx
+++ src/app/federation/page.tsx
@@ -1,4 +1,8 @@
 import FederationPage from '@/components/pages/federation/federation.page';
+import { Metadata } from 'next';
+import { federationMeta } from '@/components/seo/meta/pages';
+
+export const metadata: Metadata = federationMeta;
 
 const Federation: React.FC = () => {
   return <FederationPage />;
--- src/components/seo/meta/pages/index.ts
+++ src/components/seo/meta/pages/index.ts
@@ -1,5 +1,5 @@
 export { homeMeta } from './home.meta';
 export { aboutMeta } from './about.meta';
 export { galleryMeta } from './gallery.meta';
-export { contactsMeta } from './contacts.meta';
 export { notFoundMeta } from './not-found.meta';
+export { federationMeta } from './federation.meta';
